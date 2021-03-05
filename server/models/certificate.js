f47f05dad92c7a2d4c6cd16d56a6f006451d4039
wajdi-hamrouni <wajdihamrouni1993@gmail.com>
2021-03-05 14:26:15 +0100

    mongoAtlasConnectd

---
+++ server/models/certificate.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const Certificate = new mongoose.Schema(
+  {
+    studentFirstName: {
+      type: String,
+      //required: [true, "Please enter the student's first name"]
+    },
+    studentLastName: {
+      type: String,
+     // required: [true,"Please enter the student's last name"]
+    },
+
+    cohort: {
+      type: String,
+     // required: [true, "Please enter the student's cohort"]
+    },
+
+    graduationDate: {
+      type: Date,
+      //required: [true, "Please enter the student's cohort"]
+    },
+    certificateUrl: {
+        type: URL,
+        //required: [true, "Please enter the certificate url"]
+    },
+   timestamps: true 
+}
+);
+
+
+module.exports = mongoose.model("Certificate", Certificate);
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,63 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const path = require('path')
+// const webpack = require('webpack')
 
+// module.exports = {
+//   entry: path.resolve(__dirname, 'client/src', 'index.js'),
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'bundle.js'
+//   },
+//   devServer: {
+//     contentBase: path.resolve(__dirname, 'dist'),
+//     open: true,
+//     clientLogLevel: 'silent',
+//     port: 5000
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test:/\.(js|jsx)?$/,
+//         include: path.resolve(__dirname, 'src'),
+//         exclude: /node_modules/,
+//         use: [{
+//           loader: 'babel-loader',
+//           options: {
+//             presets: [
+//               ['@babel/preset-env', {
+//                 "targets": "defaults" 
+//               }],
+//               '@babel/preset-react',
+//             ]
+//           }
+//         }]
+//       },
+      
+//     ]
+//   },
+//   resolve: {
+//     extensions: ['*', '.js', '.jsx'],
+//   },
+// }
+
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/dist');
 module.exports = {
-    entry: path.resolve(__dirname, 'client/src', 'index.js'),
-    output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
   },
   module: {
-    rules: [
+    loaders: [
       {
-        test: /\.(jsx|js)$/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', {
-                "targets": "defaults" 
-              }],
-              '@babel/preset-react'
-            ]
-          }
-        }]
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
       }
     ]
-  },
-  
+  }
 };
